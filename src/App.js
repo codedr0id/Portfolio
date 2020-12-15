02487899195b4c66d1560539ec3349a6d1661a19
Mehul Lathi <mehullathi1999@gmail.com>
2020-12-16 02:57:18 +0530

    Adjust spacing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import About from './components/About';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../scss/About.scss';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+        <h1>about</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
