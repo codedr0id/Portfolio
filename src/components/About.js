59d9fc4a2b0b83f9d26372c56167d20d06fbc236
Mehul Lathi <mehullathi1999@gmail.com>
2020-12-18 01:43:11 +0530

    Added about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import '../scss/About.scss';
+import aboutPhoto from '../img/me-light.jpg';
 
 class About extends Component {
   render() {
     return (
-      <div className="About">
-        <h1>about</h1>
-      </div>
+      <section className="About" id="about">
+        <h1 className="section-title">About</h1>
+	        <div className="About__photo-container">
+          <img src={aboutPhoto} alt="about photo" className="About__photo" />
+        </div>
+        <div className="About__description">
+          <p>
+            My name is <span class="highlight">Mehul Lathi</span> and I'm an aspiring
+            frontend developer living in Udaipur, Rajasthan. I am pursuing my Bachelor's degree in Computer
+            Science at IIIT Vadodara and I am eager to start a career in web development.
+          </p>
+
+          <p>
+            Duis lobortis gravida erat sed faucibus. Etiam eget sapien cursus, lobortis elit sed, mattis libero. Cras facilisis accumsan lacus. Etiam ut odio tempor, pretium libero at, commodo orci. Praesent vel massa est. Curabitur neque ex, placerat a tempus in, feugiat eu leo. Donec gravida consectetur cursus. Curabitur magna nibh, rhoncus et mollis nec, congue cursus arcu.
+          </p>
+
+          <p>
+            Cras enim lacus, auctor eget leo ac, dictum elementum tortor. Donec vel posuere tortor, eget euismod nisl. Nullam ac justo eget libero tempus porta ut mollis massa. Sed feugiat ipsum at ullamcorper venenatis. Morbi id lobortis arcu. Sed luctus dui non diam maximus tincidunt. Integer ac erat tortor. Etiam tincidunt finibus ante et suscipit. Etiam tristique turpis sed pulvinar sagittis. Nullam et gravida lorem.
+          </p>
+        </div>
+        <a href="#" className="cta">
+          Resume
+        </a>
+      </section>
     );
   }
 }
