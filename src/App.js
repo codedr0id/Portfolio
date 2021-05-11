df26294d17fe15a295ce376c13d6ba75c15ffd37
Mehul Lathi <mehullathi1999@gmail.com>
2021-05-11 18:03:58 +0530

    Preloader added

--- src/App.js
+++ src/App.js
@@ -7,33 +7,59 @@ import Skills from "./components/Skills";
 import ProjectsContainer from "./components/ProjectsContainer";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
 
 import "./App.scss";
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Element name="home">
-        <Header />
-      </Element>
-      <main>
-        <Element name="about">
-          <About />
-        </Element>
-        <Element name="skills">
-          <Skills />
-        </Element>
-        <Element name="projects">
-          <ProjectsContainer />
-        </Element>
-        <Element name="contact">
-          <Contact />
-        </Element>
-      </main>
-      <Footer />
-    </div>
-  );
+  componentDidMount() {
+    this.isLoading = setTimeout(() => {
+      this.setState({ loading: false });
+    }, 1800);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.isLoading);
+  }
+
+  // timer = () =>
+  //   setTimeout(() => {
+  //     this.setState({ loading: false });
+  //   }, 3000);
+
+  render() {
+    const { loading } = this.state;
+    return loading ? (
+      <Preloader />
+    ) : (
+      <React.Fragment>
+        <div className="App">
+          <Navbar />
+          <Element name="home">
+            <Header />
+          </Element>
+          <main>
+            <Element name="about">
+              <About />
+            </Element>
+            <Element name="skills">
+              <Skills />
+            </Element>
+            <Element name="projects">
+              <ProjectsContainer />
+            </Element>
+            <Element name="contact">
+              <Contact />
+            </Element>
+          </main>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Preloader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "@fontsource/montez";
+import "../scss/Preloader-light.scss";
+
+const Preloader = () => {
+  return <div className="box"></div>;
+};
+
+export default Preloader;
